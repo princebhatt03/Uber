60bda34ac9c3c5b1d63b88336e1bb98c71c4eb68
Prince Bhatt <sharmaprince89667@gmail.com>
2024-12-21 19:17:45 +0530

    9th Commit, User and Captain's Login SignUp Done, Testing Done

--- Frontend/src/pages/CaptainSignup.jsx
+++ Frontend/src/pages/CaptainSignup.jsx
@@ -12,7 +12,7 @@ const CaptainSignup = () => {
   const submitHandler = e => {
     e.preventDefault();
     setUserData({
-      username: {
+      fullName: {
         firstName: firstName,
         lastName: lastName,
       },
